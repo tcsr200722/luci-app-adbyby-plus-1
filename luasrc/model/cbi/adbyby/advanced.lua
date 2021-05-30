c74f6c9e43c437e03e22063f1aa37d9a6cfe5540
Bob Zhang <zhang.deplives@gmail.com>
2021-05-30 14:55:35 +0800

    update

--- luasrc/model/cbi/adbyby/advanced.lua
+++ luasrc/model/cbi/adbyby/advanced.lua
@@ -2,14 +2,14 @@
 local SYS  = require "luci.sys"
 local ND = SYS.exec("cat /usr/share/adbyby/dnsmasq.adblock | wc -l")
 
-local ad_count=0
+local ad_count = 0
 if nixio.fs.access("/usr/share/adbyby/dnsmasq.adblock") then
-ad_count=tonumber(SYS.exec("cat /usr/share/adbyby/dnsmasq.adblock | wc -l"))
+ad_count = tonumber(SYS.exec("cat /usr/share/adbyby/dnsmasq.adblock | wc -l"))
 end
 
-local rule_count=0
+local rule_count = 0
 if nixio.fs.access("/usr/share/adbyby/rules/") then
-rule_count=tonumber(SYS.exec("/usr/share/adbyby/rule-count '/usr/share/adbyby/rules/'"))
+rule_count = tonumber(SYS.exec("/usr/share/adbyby/rule-count '/usr/share/adbyby/rules/'"))
 end
 
 m = Map("adbyby")
@@ -18,31 +18,31 @@ s = m:section(TypedSection, "adbyby")
 s.anonymous = true
 
 o = s:option(Flag, "block_ios")
-o.title = translate("Block Apple iOS OTA update")
+o.title = translate("Block iOS Update")
 o.default = 0
 o.rmempty = false
 
 o = s:option(Flag, "block_cnshort")
-o.title = translate("Block CNshort APP and Website")
+o.title = translate("Block CNshort Video")
 o.default = 0
 o.rmempty = false
 
 o = s:option(Flag, "cron_mode")
-o.title = translate("Update the rule at 6 a.m. every morning and restart adbyby")
+o.title = translate("Update Rules")
 o.default = 0
 o.rmempty = false
 
-o = s:option(DummyValue, "ad_data", translate("Adblock Plus Data"))
+o = s:option(DummyValue, "ad_data", translate("Rules Datebase"))
 o.rawhtml  = true
 o.template = "adbyby/refresh"
 o.value = ad_count .. " " .. translate("Records")
 
-o = s:option(DummyValue, "rule_data", translate("Subscribe 3rd Rules Data"))
+o = s:option(DummyValue, "rule_data", translate("Subscribe Rules"))
 o.rawhtml  = true
 o.template = "adbyby/refresh"
 o.value = rule_count .. " " .. translate("Records")
 
-o = s:option(Button, "delete", translate("Delete All Subscribe Rules"))
+o = s:option(Button, "delete", translate("Clear All Subscribe Rules"))
 o.inputstyle = "reset"
 o.write = function()
   SYS.exec("rm -f /usr/share/adbyby/rules/data/* /usr/share/adbyby/rules/host/*")
@@ -50,7 +50,7 @@ o.write = function()
   luci.http.redirect(luci.dispatcher.build_url("admin", "services", "adbyby", "advanced"))
 end
 
-o = s:option(DynamicList, "subscribe_url", translate("Anti-AD Rules Subscribe"))
+o = s:option(DynamicList, "subscribe_url", translate("Subscribe URL"))
 o.rmempty = true
 
 return m
--- luasrc/model/cbi/adbyby/base.lua
+++ luasrc/model/cbi/adbyby/base.lua
@@ -8,9 +8,8 @@ local NU = SYS.exec("cat /usr/share/adbyby/data/user.txt | wc -l")
 local UD = NXFS.readfile("/tmp/adbyby.updated") or "1970-01-01 00:00:00"
 
 m = Map("adbyby")
-m.title	= translate("Adbyby Plus + Settings")
 
-m:section(SimpleSection).template  = "adbyby/adbyby_status"
+m:section(SimpleSection).template = "adbyby/adbyby_status"
 
 s = m:section(TypedSection, "adbyby")
 s.anonymous = true
@@ -22,16 +21,16 @@ o.rmempty = false
 
 o = s:option(ListValue, "wan_mode")
 o.title = translate("Running Mode")
-o:value("0", translate("Global Mode (The slowest and the best effects)"))
-o:value("1", translate("Plus + Mode (Filter domain name list and blacklist website.Recommended)"))
-o:value("2", translate("No filter Mode (Must set in Client Filter Mode Settings manually)"))
+o:value("0", translate("Global Mode"))
+o:value("1", translate("Filter Mode"))
+o:value("2", translate("Manual Mode"))
 o.default = 1
 o.rmempty = false
 
 o = s:option(Button, "restart")
-o.title = translate("Adbyby and Rule state")
-o.inputtitle = translate("Update Adbyby Rules Manually")
-o.description = string.format("<strong>"..translate("Last Update Checked")..":</strong> %s", UD)
+o.title = translate("Rule State")
+o.inputtitle = translate("Update Manually")
+o.description = string.format("<strong>"..translate("Last Update")..":</strong> %s", UD)
 o.inputstyle = "reload"
 o.write = function()
 	SYS.call("rm -rf /tmp/adbyby.updated /tmp/adbyby/admd5.json && /usr/share/adbyby/adbybyupdate.sh > /tmp/adupdate.log 2>&1 &")
@@ -39,28 +38,27 @@ o.write = function()
 	HTTP.redirect(DISP.build_url("admin", "services", "adbyby"))
 end
 
-t=m:section(TypedSection,"acl_rule",translate("<strong>Client Filter Mode Settings</strong>"),
-translate("Filter mode settings can be set to specific LAN clients ( <font color=blue> No filter , Global filter </font> ) . Does not need to be set by default."))
-t.template="cbi/tblsection"
-t.sortable=true
-t.anonymous=true
-t.addremove=true
+t = m:section(TypedSection, "acl_rule", translate("<strong>Client Filter Mode Settings</strong>"), translate("Set different filter mode for LAN clients"))
+t.template = "cbi/tblsection"
+t.sortable = true
+t.anonymous = true
+t.addremove = true
 
-e=t:option(Value,"ipaddr",translate("IP Address"))
-e.width="40%"
-e.datatype="ip4addr"
-e.placeholder="0.0.0.0/0"
+e = t:option(Value, "ipaddr", translate("IP Address"))
+e.width = "40%"
+e.datatype = "ip4addr"
+e.placeholder = "0.0.0.0/0"
 luci.ip.neighbors({ family = 4 }, function(entry)
 	if entry.reachable then
 		e:value(entry.dest:string())
 	end
 end)
 
-e=t:option(ListValue,"filter_mode",translate("Filter Mode"))
-e.width="40%"
-e.default="disable"
-e.rmempty=false
-e:value("disable",translate("No filter"))
-e:value("global",translate("Global filter"))
+e = t:option(ListValue, "filter_mode", translate("Filter Mode"))
+e.width = "40%"
+e.default = "disable"
+e.rmempty = false
+e:value("disable", translate("No Filter"))
+e:value("global", translate("Global Filter"))
 
 return m
